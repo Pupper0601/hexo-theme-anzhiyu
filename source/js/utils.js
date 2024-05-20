649d0aeae46550cf22c3dafd32b834414ebe7ee6
Puppper <pupper.cheng@gmail.com>
2024-05-20 15:26:43 +0800

    修改文章中的 ::: 样式

--- source/js/utils.js
+++ source/js/utils.js
@@ -629,8 +629,7 @@ const anzhiyu = {
     var input = document.getElementsByClassName("el-textarea__inner")[0];
     if (!input) return;
     const evt = new Event("input", { cancelable: true, bubbles: true });
-    const defaultPlaceholder =
-      "昵称（请勿包含博客等字样）：\n网站地址（要求博客地址，请勿提交个人主页）：\n头像图片url（请提供尽可能清晰的图片，我会上传到我自己的图床）：\n描述：\n站点截图（可选）：\n";
+    const defaultPlaceholder = "";
     input.value = this.getConfigIfPresent(GLOBAL_CONFIG.linkPageTop, "addFriendPlaceholder", defaultPlaceholder);
     input.dispatchEvent(evt);
     input.focus();
@@ -1046,15 +1045,15 @@ const anzhiyu = {
 
     function dr_js_autofill_commentinfos() {
       var lauthor = [
-          "#author",
-          "input[name='comname']",
-          "#inpName",
-          "input[name='author']",
-          "#ds-dialog-name",
-          "#name",
-          "input[name='nick']",
-          "#comment_author",
-        ],
+        "#author",
+        "input[name='comname']",
+        "#inpName",
+        "input[name='author']",
+        "#ds-dialog-name",
+        "#name",
+        "input[name='nick']",
+        "#comment_author",
+      ],
         lmail = [
           "#mail",
           "#email",

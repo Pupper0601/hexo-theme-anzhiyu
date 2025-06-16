53ba999f7d511b03163a57cdb1c3ff2d55bec783
Pupper <pupper.cheng@gmail.com>
2025-06-16 13:37:22 +0800

    修改 ai 摘要, 更换 deepseek

--- scripts/tag/link.js
+++ scripts/tag/link.js
@@ -15,11 +15,10 @@ function link(args) {
   }
 
   return `<div calss='anzhiyu-tag-link'><a class="tag-Link" target="_blank" href="${urlFor(link)}">
-    <div class="tag-link-tips">${InsideStation ? "站内地址" : "引用站外地址"}</div>
+    <div class="tag-link-tips">${InsideStation ? "站内地址" : "🙄引用站外地址，不保证站点的可用性和安全性"}</div>
     <div class="tag-link-bottom">
-        <div class="tag-link-left" style="${
-          imgUrl ? `background-image: url(${InsideStation ? "/img/512.png" : imgUrl})` : ""
-        }">
+        <div class="tag-link-left" style="${imgUrl ? `background-image: url(${InsideStation ? "/img/512.png" : imgUrl})` : ""
+    }">
           <i class="anzhiyufont anzhiyu-icon-link" style="${imgUrl ? "display: none" : ""}"></i>
         </div>
         <div class="tag-link-right">
